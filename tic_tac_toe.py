adda4f41cd4ea8a3ab277cf0395806941f36575c
Ray Porras <rayporras@Rays-Mac-mini.local>
2018-01-26 23:00:02 -0800

    initial commit

---
+++ tic_tac_toe.py
@@ -0,0 +1,130 @@
+from IPython.display import clear_output
+import random
+
+
+def display_board(board):
+    clear_output()
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+
+def player_input():
+    marker = ' '
+    while not (marker == 'O' or marker == 'X'):
+        marker = input('Do you want to be X or O?').upper()
+
+        if marker == 'X':
+            return ('X','O')
+        else:
+            return ('O','X')
+
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+
+def win_check(board, mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
+            (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
+            (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
+            (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
+            (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
+            (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
+            (board[7] == mark and board[5] == mark and board[3] == mark) or  # diagonal
+            (board[9] == mark and board[5] == mark and board[1] == mark))  # diagonal
+
+
+def choose_first():
+    if random.randint(0, 1) == 0:
+        return 'Computer'
+    else:
+        return 'Player'
+
+
+def space_check(board, position):
+    return board[position] == ' '
+
+
+def full_board_check(board):
+    for i in range(1,10):
+        if space_check(board, i):
+            return False
+    return True
+
+
+def player_choice(board):
+    position = ' '
+    while position not in '1 2 3 4 5 6 7 8 9'.split() or not space_check(board, int(position)):
+        position = input('Choose your next position: (1-9)')
+
+    return int(position)
+
+
+def computer_choice(board):
+    position = 0
+
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = random.randint(1, 9)
+
+    return position
+
+
+print('Welcome to Tic Tac Toe!')
+
+while True:
+    # Reset the board
+    theBoard = [' '] * 10
+    player_marker = 'X'
+    computer_marker = 'O'
+    turn = choose_first()
+    print(turn + ' will go first.')
+    game_on = True
+
+    while game_on:
+        if turn == 'Player':
+            # Player1's turn.
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player_marker, position)
+
+            if win_check(theBoard, player_marker):
+                display_board(theBoard)
+                print('Congratulations! You have won the game!')
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print('The game is a draw!')
+                    break
+                else:
+                    turn = 'Computer'
+
+        else:
+            # Computer's turn.
+
+            display_board(theBoard)
+            position = computer_choice(theBoard)
+            place_marker(theBoard, computer_marker, position)
+
+            if win_check(theBoard, computer_marker):
+                display_board(theBoard)
+                print('The computer won!')
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print('The game is a tie!')
+                    break
+                else:
+                    turn = 'Player'
+
+
